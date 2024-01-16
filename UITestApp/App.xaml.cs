fdc0c55d653aa912ae6150e873df53188650fa31
юзер <@DESKTOP-RRSQIN1>
2024-01-16 10:07:59 +0300

    Добавьте файлы проекта.

---
+++ UITestApp/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace UITestApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
