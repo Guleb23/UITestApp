c813aa26b9b5bbb208ec3da5a4a837ffe2aaad56
ГЛЕБ <tonovgleb@gmail.com>
2024-01-23 02:02:20 +0300

    add api, autoritzation and captha

--- UITestApp/MainWindow.xaml.cs
+++ UITestApp/MainWindow.xaml.cs
@@ -1,13 +1,10 @@
-﻿using System.Text;
+﻿using System.Net.Http.Headers;
+using System.Net.Http;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using ElbrusAPI.Models;
+using System.Runtime.InteropServices;
+using EasyCaptcha.Wpf;
 
 namespace UITestApp
 {
@@ -16,14 +13,70 @@ namespace UITestApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        int countLogin = 0;
+        Task<IEnumerable<WorkerModel>> models;
+        string answer = "";
         public MainWindow()
         {
             InitializeComponent();
+            models = CallAPI();
+            WorkerModel work = new WorkerModel();
+            work = models.Result.FirstOrDefault();
+            MyCaptcha.CreateCaptcha(Captcha.LetterOption.Alphanumeric, 5);
+            answer = MyCaptcha.CaptchaText;
+
         }
 
         private void TextBox_GotFocus(object sender, RoutedEventArgs e)
         {
             txbLogin.BorderBrush = Brushes.White;
+        }
+
+
+        public static async Task<IEnumerable<WorkerModel>> CallAPI()
+        {
+            HttpClient httpClient = new HttpClient();
+            string baseUrl = "https://localhost:7176/ElbrusAPIUser";
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            httpClient.BaseAddress = new Uri(baseUrl);
+            var response = httpClient.GetAsync(baseUrl).Result;
+            if(response.IsSuccessStatusCode)
+            {
+                var res = response.Content.ReadAsAsync<IEnumerable<WorkerModel>>().Result;
+                return res;
+            }
+            else
+            {
+                return response.Content.ReadAsAsync<IEnumerable<WorkerModel>>().Result;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var currentUser = models.Result.FirstOrDefault(w => w.login == txbLogin.Text && w.password == txbPass.Password);
+            if(countLogin >= 2)
+            {
+                MyCaptcha.Visibility = Visibility.Visible;
+                stackCap.Visibility = Visibility.Visible;
+                if(currentUser != null && txbCap.Text == answer)
+                {
+                    MessageBox.Show("Вы успешно вошли");
+                }
+                else
+                {
+                    MessageBox.Show("aGAIN");
+                }
+
+            }else if(currentUser != null)
+            {
+                MessageBox.Show("Вы успешно вошли");
+            }
+            else
+            {
+                countLogin++;
+                MessageBox.Show("Не найдено в системе");
+            }
         }
     }
 }
---
+++ UITestApp/Models/WorkerModel.cs
@@ -0,0 +1,13 @@
+﻿namespace ElbrusAPI.Models
+{
+    public class WorkerModel
+    {
+        public int Id { get; set; }
+        public string?  FIO { get; set; }
+        public string? login { get; set; }
+        public string? password { get; set; }
+        public int RoleID { get; set; }
+        public Role Role { get; set; }
+
+    }
+}
